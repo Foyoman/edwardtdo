bbecd73d7857cf3d964c7074057e9622dc5b73a6
Edward Do <edwardtdo@gmail.com>
2023-04-14 13:37:42 -0600

    homepage and about content

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import Seo from '../components/seo'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="Home">
+      <p>
+        Hi, I'm Edward. Welcome to my site! I'm a passion-driven developer currently focused on full-stack web development but with an insatiable interest for all areas of programming :)
+      </p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
-        src="../images/clifford.jpg"
+        alt="A picture of me, Edward, at Machu Picchu posing with a thumbs up"
+        src="../images/me.jpeg"
       />
     </Layout>
   )
