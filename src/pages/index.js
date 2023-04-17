fbe45912c5265a8be86447a07fffb2a4d926712d
Edward Do <edwardtdo@gmail.com>
2023-04-17 11:23:45 -0600

    homepage update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,21 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>
-        Hi, I'm Edward. Welcome to my site! I'm a passion-driven developer currently focused on full-stack web development but with an insatiable interest for all areas of programming :)
+        Hi, I'm Edward. Welcome to my site! My two biggest loves in life are my dog Donut, and backpacking!
       </p>
       <StaticImage
         alt="A picture of me, Edward, at Machu Picchu posing with a thumbs up"
         src="../images/me.jpeg"
+        height={200}
       />
+      <h2 className="heading">Expertise</h2>
+      <p>I'm a passion-driven developer currently focused on full-stack web development but with an insatiable interest for all areas of programming :). I'm currently focused on using the most relevant and modern full-stack web dev technologies such as:</p>
+      <ul>
+        <li>React, Next, Gatsby</li>
+        <li>Vue, Nuxt</li>
+        <li>Node, Express, MongoDB</li>
+        <li>Redux, GraphQL</li>
+      </ul>
     </Layout>
   )
 }
