9412b3f91231660ce1b87c962f43533bcd76df67
Edward Do <edwardtdo@gmail.com>
2023-04-17 15:01:42 -0600

    implementing projects page in a more creative fashion

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,23 +1,21 @@
 import * as React from 'react'
 import { Link, graphql } from 'gatsby'
-import Layout from '../../components/layout'
+import Layout from '../../components/layout/layout'
 import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Travel Blog">
-      {
-        data.allMdx.nodes.map(node => (
-          <article key={node.id}>
-            <h2>
-              <Link to={`/blog/${node.frontmatter.slug}`}>
-                {node.frontmatter.title}
-              </Link>
-            </h2>
-            <p>Posted: {node.frontmatter.date}</p>
-          </article>
-        ))
-      }
+    {data.allMdx.nodes.map(node => (
+      <article key={node.id}>
+        <h2>
+          <Link to={`/blog/${node.frontmatter.slug}`}>
+            {node.frontmatter.title}
+          </Link>
+        </h2>
+        <p>Posted: {node.frontmatter.date}</p>
+      </article>
+    ))}
     </Layout>
   )
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,8 +1,30 @@
 import * as React from 'react'
-import Layout from '../../components/layout'
+import { Link } from 'gatsby'
+import Layout from '../../components/layout/layout'
 import Seo from '../../components/seo'
 
 const ProjectsPage = () => {
+  const projects = [
+    {
+      title: 'wizzy-wig',
+      slug: 'wizzy-wig',
+      date: 'March, 2023',
+      stack: ['TypeScript', 'React', 'Redux', 'MUI']
+    },
+    {
+      title: 'tonguist',
+      slug: 'tonguist',
+      date: 'September, 2022',
+      stack: ['React', 'Node', 'Express', 'MongoDB', 'MUI']
+    },
+    {
+      title: 'Brain Train',
+      slug: 'brain-train',
+      date: 'August, 2022',
+      stack: ['React', 'Firebase', 'Redux']
+    }
+  ]
+
   return (
     <Layout pageTitle="My Projects">
     </Layout>
