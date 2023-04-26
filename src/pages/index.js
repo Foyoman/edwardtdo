93e11eeb76f05a2bebc0d942a82513c8a5580e3a
Edward Do <edwardtdo@gmail.com>
2023-04-26 14:43:32 -0600

    placeholder text

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,13 +52,13 @@ const IndexPage = () => {
             About Me
           </h2>
           <p>
-            Hello! My name is Edward, and I have a passion for creating visions of highly interactive and functional web apps. My interest in web development was initiated in my early childhood days of creating Starcraft maps for local players.
+            Hello! My name is Edward, and I have a passion for creating visions of highly interactive and functional web apps.
           </p>
           <p>
-            After working in Telecommunications for almost 9 years, I decided to pursue my passion for software engineering. In mid-2022, I completed the 3-month Software Engineering Immersive course at General Assembly in Sydney.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum consequuntur ipsam ipsum culpa illo, quo eligendi, in soluta nobis corporis quam sit repellendus aliquid officiis molestiae vel, perferendis sequi reprehenderit?
           </p>
           <p>
-            My experience in leadership and consulting on the client side has helped me develop a unique and holistic perspective on customer experience and effective team communication.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ad facere officiis, fugit neque, commodi amet perspiciatis modi cupiditate dolorem voluptatibus animi accusamus quaerat est atque adipisci blanditiis fugiat veritatis.
           </p>
           <p>
             I’m currently open to job opportunities so please feel free to email or connect with me via LinkedIn.
